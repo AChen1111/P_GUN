51a94dd6231f4b25fea81f435960c6fd3ff4d1af
AChen <2946517846@qq.com>
2026-05-17 15:34:15 +0800

    修复UI显示BUG

--- Assets/Scripts/UI/Panel/MainPanel.BindComponent.cs
+++ Assets/Scripts/UI/Panel/MainPanel.BindComponent.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-// 自动生成于: 2026/5/16 17:02:47
+// 自动生成于: 2026/5/17 15:15:25
 namespace Game.UI
 {
 
 	public partial class MainPanel
 	{
 
+		private Button m_Btn_Start;
+		private Button m_Btn_Load;
+		private Button m_Btn_Setting;
+		private Button m_Btn_Quit;
 
 		private void GetBindComponents(GameObject go)
 		{
 			ComponentAutoBindTool autoBindTool = go.GetComponent<ComponentAutoBindTool>();
 
+			m_Btn_Start = autoBindTool.GetBindComponent<Button>(0);
+			m_Btn_Load = autoBindTool.GetBindComponent<Button>(1);
+			m_Btn_Setting = autoBindTool.GetBindComponent<Button>(2);
+			m_Btn_Quit = autoBindTool.GetBindComponent<Button>(3);
 		}
 	}
 }
